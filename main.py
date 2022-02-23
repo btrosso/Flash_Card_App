fad439be555fc0dd838bc1f92e7b01eb143794cb
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-23 12:57:07 -0600

    Update main.py
    finished out the project
    
    Took 1 hour 12 minutes

--- main.py
+++ main.py
@@ -6,17 +6,43 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_1 = ("Arial", 40, "italic")
 FONT_2 = ("Arial", 60, "bold")
-LANGUAGE = {"front": "French", "back": "English"}
+
+
+# Ingest CSV data
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+    to_learn = data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+current_card = {}
 
 
 # Functions
-def gen_new_word():
-    global random_word, word_dict, word_text
-    random_word = random.choice(word_dict)
-    canvas.itemconfig(word_text, text=f"{random_word['French']}")
+def next_card():
+    global current_card, flip_timer, to_learn
+    root.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_image, image=card_img_front)
+    flip_timer = root.after(3000, func=flip_card)
 
-# -------------FLIP MECHANISM-----------#
 
+def remove():
+    global current_card, to_learn
+    to_learn.remove(current_card)
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+
+
+def flip_card():
+    global current_card
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_image, image=card_img_back)
 
 
 # Set Up User Interface
@@ -24,31 +50,28 @@ root = Tk()
 root.config(width=850, height=576, padx=50, pady=50, background="black")
 timer = root.after(3000)
 
-# Ingest CSV data
-words = pandas.read_csv("data/french_words.csv")
-word_dict = words.to_dict(orient="records")
-print(word_dict)
+flip_timer = root.after(3000, func=flip_card)
 
-random_word = random.choice(word_dict)
-print(random_word)
-
-card_img = PhotoImage(file="images/card_front.png")
+card_img_front = PhotoImage(file="images/card_front.png")
+card_img_back = PhotoImage(file="images/card_back.png")
 
 canvas = Canvas(root)
-canvas.config(width=850, height=576, highlightthickness=0, background="black")
+canvas.config(width=850, height=526, highlightthickness=0, background="black")
 canvas.grid(column=0, row=0, columnspan=2)
-canvas.create_image(425, 280, image=card_img)
+card_image = canvas.create_image(400, 263, image=card_img_front)
 
-canvas.create_text(400, 150, text=f"French", font=FONT_1)
-word_text = canvas.create_text(400, 263, text=f"{random_word['French']}", font=FONT_2)
+card_title = canvas.create_text(400, 150, text="", font=FONT_1)
+card_word = canvas.create_text(400, 263, text="", font=FONT_2)
 
-cross_img = PhotoImage(file="images/right.png")
-cross_button = Button(image=cross_img, highlightthickness=0, background="black", command=gen_new_word)
-cross_button.grid(column=1, row=1)
-check_img = PhotoImage(file="images/wrong.png")
-check_button = Button(image=check_img, highlightthickness=0, background="black")
-check_button.grid(column=0, row=1)
+right_img = PhotoImage(file="images/right.png")
+check_button = Button(image=right_img, highlightthickness=0, background="black",
+                      command=lambda: [next_card(), remove()])
+check_button.grid(column=1, row=1)
+wrong_img = PhotoImage(file="images/wrong.png")
+cross_button = Button(image=wrong_img, highlightthickness=0, background="black", command=next_card)
+cross_button.grid(column=0, row=1)
 
 
+next_card()
 
 root.mainloop()
